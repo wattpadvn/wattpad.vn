09f3791365e08ce567af199f96d286689393abd7
wattpad.vn <rvgvn17@gmail.com>
2020-08-06 17:31:28 +0700

    wattpad.vn

--- lib/core/genlink.ex
+++ lib/core/genlink.ex
@@ -1,25 +1,31 @@
 defmodule WP.GenLink do
-    import Ecto.Query
-    @wp "https://wattpad.vn/"
+  import Ecto.Query
+  @wp "https://wattpad.vn/"
 
-    def seo_github do
+  def genlink do
+    case Genlinks.Repo.query("select seo, name from story s order by views desc limit 100") do
+      {:ok, data} ->
+        data
 
-        case Genlinks.Repo.query("select seo, name from story s order by views desc limit 100") do
-            {:ok, data} -> data
-            content = data.rows |> Enum.map(fn [seo, name] -> name
-                    "  - [" <> name <> "](" <> @wp <> seo <> "/)\n"
-                end)
-            |> Enum.join("")
-            genFile("top100.txt", content)
-            _ -> nil
-        end
+        content =
+          data.rows
+          |> Enum.map(fn [seo, name] ->
+            name
+            "  - [" <> name <> "](" <> @wp <> seo <> "/)\n"
+          end)
+          |> Enum.join("")
+
+        genfile("top100.txt", content)
+
+      _ ->
+        nil
     end
+  end
 
-    defp genFile(linkFile, str) do
-        str = str <> "\n"
-        {:ok, file} = File.open(linkFile, [:write])
-        IO.binwrite(file, str)
-        File.close(file)
-    end
-
+  defp genfile(linkFile, str) do
+    str = str <> "\n"
+    {:ok, file} = File.open(linkFile, [:write])
+    IO.binwrite(file, str)
+    File.close(file)
+  end
 end
